bff5cedb1cd64cb2b82ea96e9d9856d1ceaadafc
azurec80 <azurec80@gmail.com>
2025-01-07 18:59:31 +0530

    Update provider.tf

--- Test/Terraform/provider.tf
+++ Test/Terraform/provider.tf
@@ -7,9 +7,9 @@ provider "azurerm" {
 
    version = "~>2.0"
 
-  # subscription_id=""
-  #   tenant_id=""
-  #   client_id=""
+  # subscription_id="4827bd74-5114-47e1-9616-1694f0f4b4c4"
+  #   tenant_id="b3f23428-46ed-43c0-9d60-4b6e1dbf0f8a"
+  #   client_id="1c47561b-f3fa-497e-bf67-9cb6f32695dc"
   features {
   }
 }
